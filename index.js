6b7de6ec94bb308ea49078bbceda4c1236e38539
Stanley Hayford <hayfordstanley@gmail.com>
2020-09-09 11:02:23 +0000

    bug fixed

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const http = require("http");
-const socketio = require("socket.io");
-const router = require("./routes/router");
-const {getUsersInRoom, getUser, removeUser, addUser} = require("./utils/users");
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
+
+const router = require('./routes/router');
+
 const app = express();
-app.use(cors());
-app.use(router);
-
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connect", socket => {
-    //socket represents a client side socket that just logged in.
-    // console.log("We have a new connection");
+app.use(cors());
+app.use(router);
 
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-
-        if(user) {
-            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-        }
-    })
-
+io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room });
 
@@ -49,8 +38,14 @@ io.on("connect", socket => {
         callback();
     });
 
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
